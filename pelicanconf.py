b89af61acf5f70d3bc5bfa00d2a29069662d4377
Doru Barbu <barbu.doru@gmail.com>
2018-04-17 20:06:50 +0300

    Add cc-by-sa 4.0 license

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,9 +33,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-  ('Pelican', 'http://getpelican.com/'),
-  ('Python.org', 'http://python.org/'),
-  ('Jinja2', 'http://jinja.pocoo.org/'),
+  ('cc-by-sa 4.0', 'http://creativecommons.org/licenses/by-sa/4.0/'),
 )
 
 # Social widget
